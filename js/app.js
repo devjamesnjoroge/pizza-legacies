cbf41259f86e4f34fe936254f33d6f4a24dcb8c5
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-19 13:53:41 +0300

    ft: It will add delivery prompt

--- js/app.js
+++ js/app.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
   $("form").submit(function () {
     event.preventDefault();
   });
+  $("button").click(function () {
+    $(".card_container").css("display", "none");
+  });
 });
