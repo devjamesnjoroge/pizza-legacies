4983f923f3fe8c3ee114997a8790c8f69a259122
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-19 18:19:54 +0300

    ft: created constructor for user Input

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 $(document).ready(function () {
   $("form").submit(function () {
     event.preventDefault();
-  });
-  $("button").click(function () {
-    $(".card_container").css("display", "none");
+    //Collect user Inputs and place them in a constructor
+
+    function Cart(pizza_crust, pizza_size, pizza_topping) {
+      (this.pizzaCrust = pizza_crust),
+        (this.pizzaSize = pizza_size),
+        (this.pizzaToppings = pizza_topping);
+    }
+
+    var toppingArray = [];
+
+    var pTopping = $('input[name="topping"]:checked').map(function () {
+      toppingArray.push($(this).val());
+    });
+
+    var userSelection = new Cart(
+      $("#pcrust").val(),
+      $("#psize").val(),
+      toppingArray
+    );
+    alert(userSelection.pizzaToppings);
   });
 });
